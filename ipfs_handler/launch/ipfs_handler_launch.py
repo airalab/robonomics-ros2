bfd1096a543b4ace0f41aa82dd257240d5f2d60e
Fingerling42 <bermanivan42@gmail.com>
2024-02-27 16:55:21 +0200

    Added IPFS daemon starting to launch file + event handler for waiting when it ready

--- ipfs_handler/launch/ipfs_handler_launch.py
+++ ipfs_handler/launch/ipfs_handler_launch.py
@@ -1,9 +1,37 @@
+from launch import SomeActionsType
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import ExecuteProcess, RegisterEventHandler
+from launch.event_handlers import OnProcessIO
+from launch.events.process import ProcessIO
+from launch.substitutions import FindExecutable
+
+
+# Create event handler that waits for an output message and then returns actions
+def on_matching_output(match_msg: str, action: SomeActionsType):
+    def on_output(event: ProcessIO):
+        for line in event.text.decode().splitlines():
+            if match_msg in line:
+                return action
+
+    return on_output
 
 
 def generate_launch_description():
     ld = LaunchDescription()
+
+    # Action for starting IPFS daemon in console and print it output
+    ipfs_daemon = ExecuteProcess(
+        cmd=[[
+            FindExecutable(name='ipfs'),
+            ' daemon'
+        ]],
+        shell=True,
+        output='screen',
+    )
+
+    # Message from IPFS daemon if it is ready
+    ipfs_ready_msg = 'Daemon is ready'
 
     # Creating node for IPFS handler
     ipfs_handler_node = Node(
@@ -11,5 +39,19 @@ def generate_launch_description():
         executable='ipfs_handler_node'
     )
 
-    ld.add_action(ipfs_handler_node)
+    # Event for starting IPFS handler node if IPFS daemon printed ready msg
+    ipfs_daemon_event = RegisterEventHandler(
+        OnProcessIO(
+            target_action=ipfs_daemon,
+            on_stdout=on_matching_output(
+                match_msg=ipfs_ready_msg,
+                action=[
+                    ipfs_handler_node,
+                ]
+            )
+        )
+    )
+
+    ld.add_action(ipfs_daemon)
+    ld.add_action(ipfs_daemon_event)
     return ld
