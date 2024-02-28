721b060da81e5455667dcadacee9d7d26bd746e9
Fingerling42 <bermanivan42@gmail.com>
2024-02-28 15:46:35 +0200

    Added IPFS files path to param file

--- robonomics_ros2_pubsub/launch/robonomics_ros2_pubsub_launch.py
+++ robonomics_ros2_pubsub/launch/robonomics_ros2_pubsub_launch.py
@@ -1,4 +1,5 @@
 import os
+import yaml
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
@@ -8,12 +9,24 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 def generate_launch_description():
     ld = LaunchDescription()
+
     # Preparing config file with params
     config = os.path.join(
         get_package_share_directory('robonomics_ros2_pubsub'),
         'config',
-        'account_params.yaml'
+        'robonomics_params.yaml'
     )
+
+    # Get args for IPFS handler
+    with open(config, 'r') as config_file:
+        params_dict = yaml.load(config_file, Loader=yaml.SafeLoader)
+        ipfs_files_path = params_dict['/ipfs_handler_node']['ros__parameters']['ipfs_files_path']
+        if ipfs_files_path is False:
+            ipfs_launch_arguments = {}.items()
+        else:
+            ipfs_launch_arguments = {
+                'ipfs_files_path': ipfs_files_path,
+            }.items()
 
     # Creating node for sender with params from config
     sender_node = Node(
@@ -33,7 +46,8 @@ def generate_launch_description():
     ipfs_handler = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory('ipfs_handler'), 'launch'),
-            '/ipfs_handler_launch.py'])
+            '/ipfs_handler_launch.py']),
+        launch_arguments=ipfs_launch_arguments
     )
 
     # Add node to launching
